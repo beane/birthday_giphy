376f6d9346574c109a245a198bf424b6d98753e9
Brian Deane <brian.deane@getbraintree.com>
2017-05-26 22:18:15 +0000

    add good js and a stylesheet

--- public/app.js
+++ public/app.js
@@ -1,1 +1,37 @@
-console.log("GIPHY")
+var baseUrl = 'https://api.giphy.com//v1/gifs/search'
+var apiKey = 'api_key=dc6zaTOxFJmzC'
+
+$(document).ready(function() {
+  makeGiphyRequest();
+})
+
+function makeGiphyRequest() {
+	var url = baseUrl
+						+ '?q=happy+birthday&fmt=json&sort=relevant&limit=100&offset=' + getRandomNumber
+						+ '&' + apiKey;
+  $.ajax(url, {
+    success: function(data, status, xhr) {
+      url = data['data'][getRandomNumber()]['images']['original']['url']
+      loadImage(url)
+      console.log('url:', url)
+    },
+    error: function(xhr, status, error) {
+      console.log(':-(')
+      console.log('xhr:', xhr)
+      console.log('status:', status)
+    }
+  })
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100)
+}
+
+function loadImage(url) {
+  var imageEl = document.images[0]
+  var downloadingImage = new Image();
+  downloadingImage.onload = function() {
+    imageEl.src = this.src
+  }
+  downloadingImage.src = url
+}
