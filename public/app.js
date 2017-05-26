ca77c998a6667faabbc81b6c02ed0d0036693f7f
Brian Deane <brian.deane@getbraintree.com>
2017-05-26 22:59:33 +0000

    add button

--- public/app.js
+++ public/app.js
@@ -2,7 +2,8 @@ var baseUrl = 'https://api.giphy.com//v1/gifs/search'
 var apiKey = 'api_key=dc6zaTOxFJmzC'
 
 $(document).ready(function() {
-  makeGiphyRequest();
+  makeGiphyRequest()
+  $('button').click(makeGiphyRequest)
 })
 
 function makeGiphyRequest() {
